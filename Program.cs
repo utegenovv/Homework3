36570754a7bb51a02801208e85d545da872f0f68
vagap <you@example.com>
2022-12-28 18:20:01 +0300

    homework3

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+void ddd (int num )
+{
+    int sec = num /10000;
+    int fest = num % 10;
+    if(sec == fest)
+    {
+        num = num / 10;
+        int sec2 =(num / 100)% 10; 
+        int fest2 = num % 10;
+        if(sec2 == fest2)
+        {
+            Console.WriteLine("палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("не палиндромом");
+        }
+    }
+}
+
+Console.Write("Велите пятизначное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+ddd(num);
+*/
+
+/*
+ double TwoPoints(double xPos1, double yPos1, double xPos2, double yPos2 ,double zPos1 , double zPos2)
+{
+    return Math.Sqrt(Math.Pow(xPos1 - xPos2, 2) + Math.Pow(yPos1 - yPos2, 2)+ Math.Pow (zPos1 - zPos2, 2) );
+}
+
+Console.Write("Input X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input y1: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(TwoPoints(x1, y1, x2, y2, z1, z2));
+*/
+/*
+void ss (int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        int c = count * count * count;
+        Console.WriteLine($"{count} > {c}");
+        count++;
+    }
+}
+Console.WriteLine("видите число");
+int num = Convert.ToInt32(Console.ReadLine ());
+ss(num);
+*/
+
